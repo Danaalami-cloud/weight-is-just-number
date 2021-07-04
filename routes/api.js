883bd9499e4e2b29412f6968484c94b697165308
Dana Alami <dana.alami18@gmail.com>
2021-07-04 14:42:37 +0300

    fixed the routes content folder

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,64 @@
 const router = require("express").Router();
-const workout = require("../models/workout");
+const Workout = require("../models/workout");
 
-router.post("/api/workout", ({ body} , res) => {
-    workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
-router.post("/api/workout/", ({body} , res) => {
-    workout.insertMany(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(params.id, {
+    $push: { exercises: body },
+  })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
-    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
-router.get("/api/workout", (req,res) => {
-    workout.find({})
-    .sort({ date: -1})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err)
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
 });
 
--- server.js
+++ server.js
@@ -10,16 +10,19 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
 
 app.use(require("./routes/api.js"));
-app.use(require("./routes/htmlroutes"))
+app.use(require("./routes/htmlroutes"));
 
 app.listen(PORT, () => {
-    console.log(`App runing on port ${PORT}!`);
+  console.log(`App runing on port ${PORT}!`);
 });
